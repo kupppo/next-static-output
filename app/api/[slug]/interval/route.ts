e61731801ff3134515ea1061aeb5242b25591912
kupppo <kupppo@protonmail.com>
2024-09-03 17:26:07 +0100

    add time-based revalidation route

---
+++ app/api/[slug]/interval/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const dynamic = 'force-static'
+export const revalidate = 60
+export const dynamicParams = true
+
+export async function generateStaticParams() {
+  return [
+    { slug: 'hello' },
+    { slug: 'world' }
+  ]
+}
+
+export function GET(_req: NextRequest, { params }: { params: { slug: string } }) {
+  console.log(`running ${params.slug}`)
+  return NextResponse.json({
+    static: true,
+    slug: params.slug
+  })
+}
