d8ca47eea57f6871b1456dd1c37121152526b6f2
kupppo <kupppo@protonmail.com>
2024-09-03 15:28:00 +0100

    initial buttons for triggering events

---
+++ app/action.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+
+export default function Action({ children, value }: { children: React.ReactNode; value: string }) {
+  const handleAction = async (action: string) => {
+    const res = await fetch(`/api/${action}`, {
+      cache: 'no-cache',
+    })
+    console.log(res.status)
+    const payload = await res.json()
+    console.log(payload)
+  }
+  return (
+    <Button onClick={() => handleAction(value)}>{children}</Button>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,43 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Action from './action'
+import { Badge } from '@/components/ui/badge'
+import { Separator } from '@/components/ui/separator'
+
+const actions = [
+  {
+    value: "hello",
+    label: "GET /api/hello",
+    type: "Build",
+  },
+  {
+    value: "world",
+    label: "GET /api/world",
+    type: "Build",
+  },
+  {
+    value: "hello",
+    label: "GET /api/foo",
+    type: "ISR",
+  },
+]
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="p-8">
+      <section>
+        <ul>
+          {actions.map((action, index) => (
+            <li key={action.value} className="block my-2 py-1">
+              <Action value={action.value}>
+                {action.label}
+              </Action>
+              <div className="mt-2 mb-4 uppercase font-normal rounded-xl text-xs font-mono">
+                {action.type}
+              </div>
+              {index < actions.length - 1 && <Separator />}
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
   );
 }
