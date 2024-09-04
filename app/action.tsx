61b7d93e290b18a0f2001e0b570036b7d296f146
kupppo <kupppo@protonmail.com>
2024-09-04 09:12:42 +0100

    custom event for logging

--- app/action.tsx
+++ app/action.tsx
@@ -4,9 +4,21 @@ import { Button } from '@/components/ui/button'
 
 export default function Action({ children, value }: { children: React.ReactNode; value: string }) {
   const handleAction = async (action: string) => {
-    const res = await fetch(`/api/${action}`, {
+    const now = new Date().toTimeString()
+    const timestamp = now.split(' ')[0]
+    const url = `/api/${action}`
+    const res = await fetch(url, {
       cache: 'no-cache',
     })
+    const evt = new CustomEvent('log:request', {
+      detail: {
+        status: res.status,
+        method: 'GET',
+        url,
+        timestamp,
+      }
+    })
+    document.dispatchEvent(evt)
   }
   return (
     <Button onClick={() => handleAction(value)}>{children}</Button>
